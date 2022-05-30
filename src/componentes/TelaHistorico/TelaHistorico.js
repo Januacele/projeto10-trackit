1787c8d12c9904124203e3da6f90ff822da1f397
Januacele <januacelevieira@hotmail.com>
2022-05-30 00:32:52 -0300

    Tela-Historico
    Foi feita a tela de historico sem o bonus, apenas o layout fornecido pelo figma.

--- src/componentes/TelaHistorico/TelaHistorico.js
+++ src/componentes/TelaHistorico/TelaHistorico.js
@@ -1,14 +1,45 @@
+import styled from "styled-components";
 
 import Header from "../Header/Header";
+import Menu from "../TelaHabitos/Menu";
 
-export default function TelaHistorico(){
+
+export default function TelaHistorico() {    
+
     return (
         <>
-        <Header />
-        <h1> Eu sou a tela de hostorico </h1>
+            <Header />
+            <Div>
+                <h1>Histórico</h1>
+                <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+            </Div>
+            <Menu />
         </>
-    
-
-    )
-   
+    );
 }
+
+const Div = styled.div`
+
+    width: 100%;
+    height: 100vh;
+    margin-top: 70px;
+    font-family: 'Lexend Deca';
+    padding: 28px 20px 0px 20px;
+   
+
+    h1 {
+        font-weight: 400;
+        font-size: 23px;
+        line-height: 29px;
+        color: #126BA5;
+        margin-bottom: 17px;
+    }
+
+    p {
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 22px;
+        color: #666666;
+    }
+
+`;
